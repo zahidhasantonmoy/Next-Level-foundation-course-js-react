94a3802287443fce7598b4a4caeee0c3ffcc1bb7
Zahid Hasan Tonmoy <zahidhasantonmoy360@gmail.com>
2026-08-30 15:04:16 +0600

    Add student data structure with details for three students including marks and skills

---
+++ module2/project.js
@@ -0,0 +1,38 @@
+let students = [
+  {
+    id: 101,
+    name: "Rahim",
+    age: 20,
+    department: "CSE",
+    marks: {
+      bangla: 75,
+      english: 82,
+      math: 90
+    },
+    skills: ["HTML", "CSS"]
+  },
+  {
+    id: 102,
+    name: "Karim",
+    age: 22,
+    department: "EEE",
+    marks: {
+      bangla: 65,
+      english: 70,
+      math: 72
+    },
+    skills: ["JavaScript"]
+  },
+  {
+    id: 103,
+    name: "Fahim",
+    age: 21,
+    department: "CSE",
+    marks: {
+      bangla: 88,
+      english: 91,
+      math: 85
+    },
+    skills: ["PHP", "MySQL"]
+  }
+];
