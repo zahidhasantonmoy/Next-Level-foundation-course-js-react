8e61387bf83cfdfd06fb702a605d16bea980c7d5
Zahid Hasan Tonmoy <zahidhasantonmoy360@gmail.com>
2026-08-30 16:03:46 +0600

    Enhance findStudent function and implement calculateResult for student grading

--- module2/project.js
+++ module2/project.js
@@ -46,9 +46,11 @@ function displayStudents(){
 displayStudents();
 
 let id=null;
-let isfound=false;
+
 function findStudent(id){
+    let isfound=false;
     for(student of students){
+        
         if( student.id==id){
             console.log(` ID: ${student.id} Name :${student.name} Department: ${student.department}`);
             isfound=true;
@@ -57,9 +59,72 @@ function findStudent(id){
         
 
     }
-        if(!isfound=false){
+        if(!isfound){
             console.log(" Student not found");
         }
 }
 
 findStudent(103);
+
+
+
+// nested marks থেকে total বের করবে
+// average বের করবে
+// average 80+ → "Excellent"
+// average 60+ → "Good"
+// otherwise → "Need Improvement"
+
+// 👉 এখানে অবশ্যই nested object + for...in + function ব্যবহার করবে।
+function calculateResult(student){
+ let total=0;
+ let count=0;
+ for(let subject in student.marks){
+ total +=student.marks[subject];
+ count++;
+ }
+console.log(`Total:${total}`);
+
+let avg= total/count;
+console.log(`AVG:${avg}`);
+if(avg>=80){
+    console.log("Excelent");
+}
+else if ( avg>=60){
+    console.log("Good");
+}
+else{
+    console.log("Need Improvement");
+}
+}
+calculateResult(students[0]);
+students[2].skills.pop();
+students[2].skills.push("git");
+students[2].skills.splice(1,0, "React");
+students[2].skills.shift();
+students[2].skills.unshift("HTML");
+console.log(students[2]);
+
+
+function addStudent(name, age, department){
+    let newid=null;
+   
+    newid=students[students.length - 1].id+1;
+    let newstudent={
+        id: newid,
+    name: name,
+    age: age,
+    department: department,
+    marks: {
+      bangla: 0,
+      english: 0,
+      math: 0
+    },
+    skills: ["PHP", "MySQL"]
+  }
+
+    }
+   
+
+
+addStudent("Zahid",25,"CSE");
+console.log(students)
