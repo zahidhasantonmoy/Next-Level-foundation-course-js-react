d6cf25ccb950a4e8a2d5b8a2acc4726212315c41
Zahid Hasan Tonmoy <zahidhasantonmoy360@gmail.com>
2026-08-31 13:38:45 +0600

    Refactor generateExpenseReport function to include highest expense calculation and clean up commented code

--- module3/projects.js
+++ module3/projects.js
@@ -29,19 +29,19 @@ const expenses = [
   }
 ];
 
-// let paid=expenses.filter((item)=>item.paid==true);
-// console.log(paid);
+let paid=expenses.filter((item)=>item.paid==true);
+console.log(paid);
 
-// let newTitle=expenses.map((item)=>item.title);
-// console.log(newTitle);
-// let newAmount=expenses.reduce((accu,curr)=> {
-//   return accu.amount>curr.amount ? accu : curr ;
-// },expenses[0]);
-// console.log(newAmount);
-// let categories=expenses.map((item)=> item.category);
+let newTitle=expenses.map((item)=>item.title);
+console.log(newTitle);
+let newAmount=expenses.reduce((accu,curr)=> {
+  return accu.amount>curr.amount ? accu : curr ;
+},expenses[0]);
+console.log(newAmount);
+let categories=expenses.map((item)=> item.category);
 
-// let isFood= categories.includes("Food");
-// console.log(isFood);
+let isFood= categories.includes("Food");
+console.log(isFood);
 
 let {id,title,amount}=expenses[0];
 console.log(id,title,amount);
@@ -63,7 +63,55 @@ const user = {
 };
 
 
+
+
 let newcity=user.profile?.city;
 console.log(newcity);
 let newphone=user.profile?.phone?? "Not available";
 console.log(newphone);
+
+
+
+function calculateTotal(...amounts){
+  // console.log(amounts);
+  let total=0;
+  amounts.map((num)=> total +=num);
+return total;
+}
+function calculateTotal(...amounts){
+  // console.log(amounts);
+  let total=0;
+  amounts.forEach((num)=>{
+    total +=num
+  });
+return total;
+}
+function calculateTotal(...amounts){
+  // console.log(amounts);
+  let total=0;
+  return amounts.reduce((num,total )=>{
+   return  total +num;
+  },0);
+
+}
+
+console.log(calculateTotal(4,5,6,7));
+console.log(calculateTotal(100, 200, 300, 50));
+
+generateExpenseReport(expenses)
+
+
+function generateExpenseReport(expenses){
+
+let total=expenses.reduce((sum,item)=>{
+  return item.amount+sum;
+},0);
+console.log(total);
+let avg=total/expenses.length;
+console.log(avg);
+  let high=expenses.reduce((sum,item)=> {
+    return item.amount>sum ? item.amount : sum;
+  },expenses[0].amount);
+  console.log(high);  
+  }
+  generateExpenseReport(expenses);
